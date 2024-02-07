33122bcd1d3b895aa60998c0a7ea41bd26234101
quangphu192 <quangphuminh1922000@gmail.com>
2024-02-08 01:34:36 +0700

    Update AllMovies.kt

--- src/en/allmovies/src/eu/kanade/tachiyomi/animeextension/en/allmovies/AllMovies.kt
+++ src/en/allmovies/src/eu/kanade/tachiyomi/animeextension/en/allmovies/AllMovies.kt
@@ -26,7 +26,7 @@ class AllMovies : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
 
     override val name = "AllMoviesForYou"
 
-    override val baseUrl = "https://allmoviesforyou.net"
+    override val baseUrl = "https://animevietsub.io/"
 
     override val lang = "en"
 
